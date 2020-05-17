8324a6d1a8c9780361bde887162f7ae6289ac3ce
Guilherme de Almeida <guilhermeaf11@hotmail.com>
2020-05-17 16:47:03 -0300

    projeto que vai disponibilizar os recursos para o dev que implementar a arquitetura

--- arquitetura-react/src/index.js
+++ arquitetura-react/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import thunk from "redux-thunk";
+import reducers from "./connection/reducers/index.js";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./arq/module/app/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let store = createStore(reducers, applyMiddleware(thunk))
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
